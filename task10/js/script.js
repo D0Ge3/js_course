b3c44f3d82501622021d69fdac4387ad4257169d
Nikita <fnik340@gmail.com>
2019-09-13 23:34:34 +0300

    task10 is ready

---
+++ task10/js/script.js
@@ -0,0 +1,88 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+    'use strict';
+    let tab = document.querySelectorAll(".info-header-tab"),
+        info = document.querySelector(".info-header"),
+        tabContent = document.querySelectorAll(".info-tabcontent");
+
+    function hideTabContent(a) {
+        for(let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove("show");
+            tabContent[i].classList.add("hide");
+        }
+    }
+    
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains("hide")) {
+            tabContent[b].classList.remove("hide");
+            tabContent[b].classList.add("show");
+        }
+    }
+    info.addEventListener('click', function(event) {
+        let target = event.target;//ДЕЛЕГИРОВАНИЕ СОБЫТИЙ
+        if (target && target.classList.contains('info-header-tab')) {//ДЕЛЕГИРОВАНИЕ СОБЫТИЙ
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    })
+    //timer
+
+    let deadline = '2019-10-13';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    function setClock(id, endTime) {
+        
+        let t1 = getTimeRemaining(endTime),
+            timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            if (t1.total <= 0) {
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+                return;
+            }
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            IFtimeOneDigitAddZero(hours, t.hours);
+            IFtimeOneDigitAddZero(minutes, t.minutes);
+            IFtimeOneDigitAddZero(seconds, t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        function IFtimeOneDigitAddZero(typeTime,time) {
+            if (time < 10) {
+                typeTime.textContent ="0" + time;
+            } else {
+                typeTime.textContent = time;
+            }
+        }
+        }
+
+    }
+
+    setClock('timer', deadline);
+} )
